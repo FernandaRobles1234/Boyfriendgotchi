396aa3f20854ebc5063b6b699cb09ba3c11ff385
fernandarobles1234 <fernandarobles1234@gmail.com>
2024-05-14 14:45:55 -0400

    Stable version

--- Assets/Scripts/Controllers/AIController.cs
+++ Assets/Scripts/Controllers/AIController.cs
@@ -7,9 +7,10 @@ public class AIController : MonoBehaviour
     private StateMachine _sm;
     private CharacterMotor _motor;
 
-    public float _sleepyMax;
+    public float _sleepyMax = 10;
     public float _sleepy= 0;
-    
+    public float _wanderingChangeTime = 5;
+    public Vector2 _wanderingWithin = new Vector2(5, 20);
 
     private void Awake() 
     { 
--- Assets/Scripts/StateMachine/WanderingState.cs
+++ Assets/Scripts/StateMachine/WanderingState.cs
@@ -1,85 +1,65 @@
 using BehaviorDesigner.Runtime.Tasks.Unity.UnityTime;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WanderingState : State
 {
     CharacterMotor _charMotor;
-    Animator _animator;
     AIController _controller;
+    Animator _animator;
+    InitPathFinding _initPathFinding;
 
-    private InitPathFinding _initPathFinding;
+    private float _timeSinceLast= 0.0f;
+    private int _iPath = 0;
     private List<PathNode> _pathToRandomPos;
 
-
-
-    public const float _time= 5.0f;
-    private float _timeSinceLast= 6.0f;
-    private int _iPath = 0;
+    private int _offset= -1;
 
     public WanderingState(GameObject go, StateMachine sm) : base(go, sm)
     {
     }
-
     public override void Enter()
     {
         _charMotor = _go.GetComponent<CharacterMotor>();
         _animator = _go.GetComponent<Animator>();
         _controller = _go.GetComponent<AIController>();
-
         _initPathFinding = GameObject.FindObjectsOfType<InitPathFinding>()[0];
-    }
 
+        _timeSinceLast = _controller._wanderingChangeTime + 1;
+    }
 
     public override void FixedUpdate()
     {
         _timeSinceLast += Time.fixedDeltaTime;
+        _controller._sleepy += Time.deltaTime;
+
         Vector3 characterPosition = _go.transform.position;
         Vector3 _toPosDir= Vector3.zero;
         
         // Pick and create a new path.
-        if (_time < _timeSinceLast)
+        if (_controller._wanderingChangeTime < _timeSinceLast)
         {
-            Vector2 within = new Vector2(5, 20);
-            Vector3 randomPos = _initPathFinding._pathFinding.RandomWorldPositionWithin(characterPosition.x, characterPosition.y, within);
+            Vector3 randomPos = _initPathFinding._pathFinding.RandomWorldPositionWithin(characterPosition.x, characterPosition.y + _offset, _controller._wanderingWithin);
 
-            characterPosition = _go.transform.position;
-
-            _pathToRandomPos = _initPathFinding._pathFinding.FindPath(characterPosition.x, characterPosition.y - 1, randomPos.x, randomPos.y);
+            _pathToRandomPos = _initPathFinding._pathFinding.FindPath(characterPosition.x, characterPosition.y + _offset, randomPos.x, randomPos.y);
 
             _timeSinceLast = 0.0f;
             _iPath = 0;
         }
 
-
         // Move the most inmediate cell of the grid's path.
         if (_iPath < _pathToRandomPos.Count)
         {
             PathNode firstNode = _pathToRandomPos[_iPath];
-            characterPosition = _go.transform.position;
 
-            _toPosDir = _initPathFinding._pathFinding._grid.GetWorldPosition(firstNode._x, firstNode._y) - characterPosition - new Vector3(0, -1, 0);
+            _toPosDir = _initPathFinding._pathFinding._grid.GetWorldPosition(firstNode._x, firstNode._y) - characterPosition - new Vector3(0, _offset, 0);
             _charMotor._moveDir = _toPosDir;
             _charMotor._moveDir.Normalize();
         }
 
-        // Handle animations.
-        if (_charMotor.IsMoving())
-        {
-            _animator.SetBool("isWalking", true);
-            _animator.SetFloat("Xinput", _charMotor._moveDir.x);
-            _animator.SetFloat("Yinput", _charMotor._moveDir.y);
-        }
-        else
-        {
-            _animator.SetBool("isWalking", false);
-        }
-
-
         // Handle conditions for the state management.
-        _controller._sleepy += Time.deltaTime;
-
         if(_controller._sleepy > _controller._sleepyMax)
         {
             _sm._CurState = new SleepingState(_go, _sm);
@@ -98,6 +78,18 @@ public class WanderingState : State
                 _iPath++;
             }
 
+        }
+
+        // Handle animations.
+        if (_charMotor.IsMoving())
+        {
+            _animator.SetBool("isWalking", true);
+            _animator.SetFloat("Xinput", _charMotor._moveDir.x);
+            _animator.SetFloat("Yinput", _charMotor._moveDir.y);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", false);
         }
 
     }
