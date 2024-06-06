085b4b67584bdb6260197b38d50f7d88f1e364b3
fernandarobles1234 <fernandarobles1234@gmail.com>
2024-06-06 09:09:34 -0400

    bug update

--- Assets/Scripts/StateMachine/SleepyState.cs
+++ Assets/Scripts/StateMachine/SleepyState.cs
@@ -41,7 +41,7 @@ public class SleepyState : State
         Vector3 characterPosition = _go.transform.position;
         Vector3 bedPosition = _BedObjects[0].transform.position;
 
-        _pathToBed = _initPathFindings[0]._pathFinding.FindPath(characterPosition.x, characterPosition.y - 1, bedPosition.x, bedPosition.y);
+        _pathToBed = _initPathFindings[0]._pathFinding.FindPath(characterPosition.x, characterPosition.y - 2, bedPosition.x, bedPosition.y);
 
         _characterZone= _go.GetComponent<BoxCollider2D>();
         _bedZone= _BedObjects[0].GetComponent<BoxCollider2D>();
@@ -62,7 +62,7 @@ public class SleepyState : State
             PathNode firstNode = _pathToBed[_i];
             Vector3 characterPosition = _go.transform.position;
 
-            _moveDir = _initPathFindings[0]._pathFinding._grid.GetWorldPosition(firstNode._x, firstNode._y) - characterPosition - new Vector3(0, -1, 0);
+            _moveDir = _initPathFindings[0]._pathFinding._grid.GetWorldPosition(firstNode._x, firstNode._y) - characterPosition - new Vector3(0, -2, 0);
             _charMotor._moveDir = _moveDir;
             _charMotor._moveDir.Normalize();
         }
