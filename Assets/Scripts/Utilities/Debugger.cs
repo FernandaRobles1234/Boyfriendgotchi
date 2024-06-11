a8e1a5f55bd48cbc26418b5a52b24d7172d3f27d
fernandarobles1234 <fernandarobles1234@gmail.com>
2024-06-11 17:25:11 -0400

    Creation of a debugger object

---
+++ Assets/Scripts/Utilities/Debugger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugger : MonoBehaviour
+{
+
+    private BoyfriendObject[] _BoyfriendObjects;
+    private BoyfriendObject _BoyfriendObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Debug Running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _BoyfriendObjects = GameObject.FindObjectsOfType<BoyfriendObject>();
+        
+        if (_BoyfriendObjects.Length == 0)
+        {
+            Debug.Log("No boyfriend object.");
+        }
+
+    }
+}
